de58aac9bbdb6c274201fce3ec8fcd90f157f2c5
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-04-04 16:56:34 -0500

    Added icons and initial coloring

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,15 @@ import './Header.css'
 function Header (props) {
   return (
     <header className="App-header">
-      <h3>
-        Dope Project
-      </h3>
+      <div className="Header">
+        <h3 id="title">
+          <img id="header-icon"
+            src="https://image.flaticon.com/icons/svg/1568/1568442.svg"
+            alt="MuseBoard" />
+          MuseBoard
+        </h3>
+      </div>
+      
     </header>
   )
 }
--- src/components/Muse-Board/Post/Post.js
+++ src/components/Muse-Board/Post/Post.js
@@ -45,13 +45,26 @@ class Post extends Component {
             placeholder='Edit post here'/> : 
           <p>{comment}</p>
         }
-        <span>
-          <button
-            onClick={() => this.setState({editting: !editting, comment})}
-          >Edit</button>
-          <button 
-            onClick={() => deletePost(id)}
-          >Delete</button>
+        <span className="Post-btn-container">
+          <div>
+            <button className="Post-btn">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-2z"/></svg>
+            </button> 0
+          </div>
+          <div>
+            <button
+              className="Post-btn"
+              onClick={() => this.setState({editting: !editting, comment})}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+            </button>
+            <button 
+              className="Post-btn"
+              onClick={() => deletePost(id)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+            </button>
+          </div>
         </span>
       </article>
     )
